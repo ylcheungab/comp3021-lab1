08464ceca0d05ed860cc5d7f39682923e3af1ef4
ylcheungab <ylcheungab@connect.ust.hk>
2015-02-11 12:36:29 +0800

    Update HelloWorld.java
    trying pull

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args){
 		//the output
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World for testing");
 		
 	}
 	
